f151fcb5dd90b904f01ca26ba56d587e7de39c64
nurbxfit <nurbxfit@gmail.com>
2025-09-05 18:32:04 +0800

    testing the props and update readme with desclaimer

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -1,5 +1,5 @@
 // src/client/pages/About.tsx
-export const About = () => {
+export const About = (props: any) => {
     return (
         <div className="max-w-2xl mx-auto py-12 px-6 text-center">
             <h1 className="text-4xl font-bold mb-4">About This Project</h1>
@@ -10,6 +10,10 @@ export const About = () => {
             <p className="text-gray-500">
                 The goal is to demonstrate how server-rendered HTML can seamlessly
                 hydrate into a live React app with state, navigation, and UI components.
+            </p>
+            <hr />
+            <p className="text-gray-600">
+                {props.message}
             </p>
         </div>
     )
--- src/workers/index.tsx
+++ src/workers/index.tsx
@@ -7,7 +7,7 @@ const app = new Hono()
 app.use(reactSSR())
 
 app.get("/", (c) => c.reactRender("home", { title: "App Root" }))
-app.get("/about", (c) => c.reactRender("about", { title: "App Root" }))
+app.get("/about", (c) => c.reactRender("about", { title: "App Root", props: { message: "Hello to the props" } }))
 // 404 fallback — must be at the end
 app.notFound((c) => {
   return c.reactRender("notFound", { title: "404 - Not Found" })
